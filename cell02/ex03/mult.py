505b33cc35e3bf27b28c9528e84fad4fcf05b900
no1tcelfer <iamnhamth7451@gmail.com>
2026-04-24 09:02:55 +0000

    23

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,4 +1,9 @@
 one = int(input("Enter the first number: "))
 two = int(input("Enter the second number: "))
 result = one * two
-print(result)
+if result > 0:
+    print("The result is positive")
+elif result < 0:
+    print("The result is negative")
+else:
+    print("The result is positive and negative")
