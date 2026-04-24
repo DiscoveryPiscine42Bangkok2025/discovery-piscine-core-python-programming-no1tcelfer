faeeb232248cb63b5d7e98ae4f2f2f114e5e348e
no1tcelfer <iamnhamth7451@gmail.com>
2026-04-24 15:26:27 +0000

    wdsa# Please enter the commit message for your changes. Lines starting

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -2,6 +2,5 @@ number = int(input("Enter a number less than 25"))
 while number < 26:
       print("Inside the loop, my variable is", number)
       number += 1
-
-    number = int(input("Enter a number less than 25"))
-input(error)
+else:
+  input("error")
