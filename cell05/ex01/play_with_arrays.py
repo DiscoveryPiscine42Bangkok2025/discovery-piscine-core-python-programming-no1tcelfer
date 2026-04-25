c83ad5680c29356c7b89b486dec23d3058843a5a
no1tcelfer <iamnhamth7451@gmail.com>
2026-04-25 05:04:16 +0000

    U R MY SPECIAL

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,5 @@
+old = [5,4,781,5,123]
+new = [x + 2 for x in old]
+
+print("Original array:", old)
+print("New array: ", new)
---
+++ cell05/ex02/play_with_arrays++.py
@@ -0,0 +1,7 @@
+old = [5,4,781,5,123]
+new =[]
+for number in old:
+    if number >5:
+      new.append(number + 2)
+print(old)
+print(new)
